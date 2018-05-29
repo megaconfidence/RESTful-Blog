defda7e1f1d34c99611e903e589eef52acbd4b1a
Confidence Okoghenun <confidenceboi@gmail.com>
2018-05-29 23:47:50 +0100

    First Commit
    This is the first commit that was made
    after initializing the repo.
    
    *The dependencies were installed and index
     route was created.
    *This has made the app ready for use.
    *Next up would be adding the new route.
    
    [closes-Bug: #001]

---
+++ app.js
@@ -0,0 +1,57 @@
+let express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  path = require('path'),
+  methodOverride = require('method-override'),
+  expressSanitizer = require('express-sanitizer');
+  
+
+mongoose.connect('mongodb://localhost/blog');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
+app.use(expressSanitizer());
+// app.use(express.static(path.join(__dirname,'public')))
+
+let blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now}
+});
+
+let Blog = mongoose.model('Blog', blogSchema);
+
+// Blog.create({
+//   title: 'Test blog',
+//   image: 'js.jpg',
+//   body: 'This is the best blog ever!!!'
+// }, (err, blog) => {
+//   if(err) console.log('AN ERROR OCCURED WHILE CREATING THIS BLOG');
+//   else { console.log('BLOG WAS SUCESSFULLY CREATED!')
+//    console.log(blog);}
+// });
+
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+});
+
+app.get('/blogs', (req, res) => {
+  Blog.find({}, (err, blogs) => {
+    if(err) console.log('AN ERROR OCCURED WHILE FINDING THE BLOGS');
+    else {
+      console.log('BLOGS WERE SUCESSFULLY FOUND')
+      res.render('index',{blogs: blogs});
+    }
+  })
+});
+
+
+
+app.listen(3000, () => {
+  console.log('Blog server is listening on port 3000');
+});
